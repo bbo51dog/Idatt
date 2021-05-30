67296e89371b860d0c3b5e099c82521fac228636
bbo51dog <bbo51dog@gmail.com>
2021-05-31 00:36:24 +0900

    Update tlexer.nim

--- tests/tlexer.nim
+++ tests/tlexer.nim
@@ -1,19 +1,23 @@
 import unittest
 import lexer/lexer, lexer/token
 
+proc assertToken(expect: Token, passed: Token) =
+  doAssert expect[] == passed[], $expect.tokenType & "(value=\"" &
+      expect.value & "\") expected, " & $passed.tokenType & "(value=\"" &
+      passed.value & "\") passed"
+
 suite "Lexer":
   test "tokenize calculation":
-    let tokens = "(123 + 2 ) * 3".tokenizeString
+    let tokens = "(123 + 456 ) * 7".tokenizeString
     let expectTokens = [
       TokenType.LParen.newToken,
       TokenType.Int.newToken($123),
       TokenType.Add.newToken,
-      TokenType.Int.newToken($2),
+      TokenType.Int.newToken($456),
       TokenType.RParen.newToken,
       TokenType.Mul.newToken,
-      TokenType.Int.newToken($3),
+      TokenType.Int.newToken($7),
     ]
     for i, token in tokens:
-      let expectToken = expectTokens[i]
-      doAssert token[] == expectToken[], $expectToken.tokenType &
-          " expected, " & $token.tokenType & " passed"
+      assertToken(expectTokens[i], token)
+
