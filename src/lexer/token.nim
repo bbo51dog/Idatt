ec78bd668c26628819c97ff7b6c714294a888b0f
bbo51dog <bbo51dog@gmail.com>
2021-05-31 00:36:42 +0900

    Add reserved words

--- src/lexer/token.nim
+++ src/lexer/token.nim
@@ -28,22 +28,36 @@ type
     GT
     LE
     LT
+    And
+    Or
     Let
     Var
+    Const
     If
     Else
     Elif
+    While
+    For
+    Fun
+    Return
 
   Token* = ref object
     tokenType*: TokenType
     value*: string
 
 const RESERVED* = {
+  "and": TokenType.And,
+  "or": TokenType.Or,
   "let": TokenType.Let,
   "var": TokenType.Var,
+  "const": TokenType.Const,
   "if": TokenType.If,
   "else": TokenType.Else,
   "elif": TokenType.Elif,
+  "While": TokenType.While,
+  "for": TokenType.For,
+  "fun": TokenType.Fun,
+  "return": TokenType.Return,
 }.toTable
 
 proc newToken*(tokenType: TokenType, value: string = ""): Token =
