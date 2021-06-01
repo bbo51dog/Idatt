0eaefb5b925331461ae8ce75324daa0671ecd667
bbo51dog <bbo51dog@gmail.com>
2021-06-02 01:49:42 +0900

    Parse add

--- src/parser/node.nim
+++ src/parser/node.nim
@@ -1,7 +1,28 @@
 type
   NodeType* = enum
+    IntLiteral
+
     Add
 
   Node* = ref object of RootObj
+    nodeType*: NodeType
+
+  TerminalNode* = ref object of Node
+    value: string
+
+  NonTerminalNode* = ref object of Node
+    left*: Node
+    right*: Node
 
   AstRoot* = seq[Node]
+
+proc newTerminalNode*(nodeType: NodeType, value: string): TerminalNode =
+  new result
+  result.nodeType = nodeType
+  result.value = value
+
+proc newNonTerminalNode*(nodeType: NodeType, left: Node, right: Node): NonTerminalNode =
+  new result
+  result.nodeType = nodeType
+  result.left = left
+  result.right = right
--- src/parser/parser.nim
+++ src/parser/parser.nim
@@ -13,10 +13,14 @@ proc newParser(tokens: seq[Token]): Parser
 proc readToken(parser: Parser): Token
 proc peekToken(parser: Parser): Token
 proc atEnd(parser: Parser): bool
+proc peekExpect(parser: Parser, tokenType: TokenType): bool
 
+proc parseExpr(parser: Parser): Node
+proc parseAdd(parser: Parser): Node
 
 proc parse*(tokens: seq[Token]): AstRoot =
   let parser = tokens.newParser
+  parser.root.add parser.parseExpr
   parser.root
 
 proc newParser(tokens: seq[Token]): Parser =
@@ -35,4 +39,19 @@ proc peekToken(parser: Parser): Token =
     TokenType.EOF.newToken
 
 proc atEnd(parser: Parser): bool =
-  parser.peekToken.tokenType == TokenType.EOF
+  parser.peekExpect(TokenType.EOF)
+
+proc peekExpect(parser: Parser, tokenType: TokenType): bool =
+  parser.peekToken.tokenType == tokenType
+
+proc parseExpr(parser: Parser): Node =
+  parser.parseAdd
+
+proc parseAdd(parser: Parser): Node =
+  if parser.peekExpect(TokenType.Int):
+    let left = NodeType.IntLiteral.newTerminalNode(parser.readToken.value)
+    discard parser.peekExpect(TokenType.Add)
+    discard parser.readToken
+    if parser.peekExpect(TokenType.Int):
+      let right = NodeType.IntLiteral.newTerminalNode(parser.readToken.value)
+      return NodeType.Add.newNonTerminalNode(left, right)
